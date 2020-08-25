b2d8cd38b0e40268888d26fed971855fb27fae27
Scott Buonya <scottbuonya@gmail.com>
2020-08-25 19:17:00 -0400

    deleted some code to improve visibility

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // array of questions
 // for user
-const questions = [
-    {
+const questions = [{
         type: "input",
         message: "What is your Github?",
         name: "github"
@@ -41,7 +40,7 @@ const questions = [
     {
         type: "list",
         message: "Please select a type of license.",
-        choices:["MIT", "APACHE2.0", "GPL3.0", "BSD3", "None"],
+        choices: ["MIT", "APACHE2.0", "GPL3.0", "BSD3", "None"],
         name: "license"
     },
     {
@@ -79,64 +78,10 @@ function writeToFile(fileName, data) {
 
 inquirer.prompt(questions).then(response => {
     console.log(response);
-    
-    writeToFile("READMEGEN.md", generateMarkDown({...response}))
-    
-    
+
+    writeToFile("READMEGEN.md", generateMarkDown({
+        ...response
+    }))
+
+
 });
-
-
-    // alternate way I did to write the code
-    // function to initialize program
-    
-    
-    
-
-
-    // let readme = "";
-    
-    // fs.appendFile("README.md", `# ${response.title}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.description}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `- ${response.table}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.installation}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.usage}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.license}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.contributing}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.tests}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
-    // fs.appendFile("README.md", `### ${response.questions}\n`, err => {
-    //     if (err) {
-    //         return console.log(err);
-    //     }    
-    // });
